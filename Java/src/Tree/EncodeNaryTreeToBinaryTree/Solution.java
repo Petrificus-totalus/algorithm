abeb329b51577937a66992b48b131c897f595202
Petrificus-totalus <1966710331@qq.com>
2022-03-09 19:38:50 +0800

    EncodeNaryTreeToBinaryTree

---
+++ Java/src/Tree/EncodeNaryTreeToBinaryTree/Solution.java
@@ -0,0 +1,81 @@
+package Tree.EncodeNaryTreeToBinaryTree;
+
+import java.util.List;
+import java.util.ArrayList;
+
+
+public class Solution {
+
+  public static class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {
+    }
+
+    public Node(int _val) {
+      val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+      val = _val;
+      children = _children;
+    }
+  };
+
+
+  public static class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+      val = x;
+    }
+  }
+
+  // Encodes an n-ary tree to a binary tree.
+  public TreeNode encode(Node root) {
+
+    if(root==null){
+      return null;
+    }
+    TreeNode head = new TreeNode(root.val);
+    head.left = en(root.children);
+    return head;
+  }
+
+  private TreeNode en(List<Node> children) {
+    TreeNode head = null;
+    TreeNode cur  = null;
+    for(Node child: children){
+      TreeNode tNode = new TreeNode(child.val);
+      if(head==null){
+        head = tNode;
+      }else{
+        cur.right = tNode;
+      }
+      cur = tNode;
+      cur.left = en(child.children);
+    }
+    return head;
+  }
+
+  // Decodes your binary tree to an n-ary tree.
+  public Node decode(TreeNode root) {
+    if (root == null) {
+      return null;
+    }
+    return new Node(root.val, de(root.left));
+  }
+
+  public List<Node> de(TreeNode root) {
+    List<Node> children = new ArrayList<>();
+    while (root != null) {
+      Node cur = new Node(root.val, de(root.left));
+      children.add(cur);
+      root = root.right;
+    }
+    return children;
+  }
+}
